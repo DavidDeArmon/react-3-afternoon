40e0f90c80c4a50319fe93aeb383fbda7ea53390
David DeArmon <david.dearmon@gmail.com>
2018-09-11 17:22:20 -0500

    solved all problems including black diamond

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Post from './Post/Post'
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+
+const BASE_URL = "https://practiceapi.devmountain.com/api"
 
 class App extends Component {
   constructor() {
@@ -16,22 +20,38 @@ class App extends Component {
     this.updatePost = this.updatePost.bind( this );
     this.deletePost = this.deletePost.bind( this );
     this.createPost = this.createPost.bind( this );
+    this.searchPosts=this.searchPosts.bind(this)
   }
   
   componentDidMount() {
-
+    axios.get(BASE_URL+'/posts').then(results=>{
+    console.log(results)
+    this.setState({posts:results.data})
+    }
+    )
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(BASE_URL+'/posts?id='+id,{text}).then(results=>{
+      console.log(results)
+      this.setState({posts: results.data})
+    })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(BASE_URL+'/posts?id='+id).then(results=>{
+      console.log(results)
+      this.setState({posts:results.data})
+    })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(BASE_URL+'/posts?id=',{text}).then(results=>{
+      this.setState({posts:results.data})
+    })
+  }
+  searchPosts(newArr){
+    this.setState({posts:newArr})
   }
 
   render() {
@@ -39,12 +59,18 @@ class App extends Component {
 
     return (
       <div className="App__parent">
-        <Header />
+        <Header posts={posts} searchPosts={this.searchPosts}/>
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+          {
+            posts.map((e)=>{
+              console.log(e)
+              return <Post key={e.id}  text={e.text} date={e.date} id={e.id} updatePostFn={this.updatePost} deletePostFn={this.deletePost}></Post>
+            })
+
+          }
         </section>
       </div>
     );
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './Search.css';
 
@@ -7,14 +8,44 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor(){
+    super()
+    this.state={
+      userinput:'',
+      posts:[]
+    }
+    this.filtersPosts=this.filtersPosts.bind(this)
+    this.componentDidMount=this.componentDidMount.bind(this)
+    this.handleChange=this.handleChange.bind(this)
+  }
+   componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts').then(results=>{
+      console.log(results)
+      this.setState({posts:results.data})
+    })
+   }
+   
+handleChange(event){    
+    this.setState({userinput:event})
+  }
+
+  filtersPosts(){
+  let newArr=this.state.posts
+ 
+  newArr=newArr.filter(post=>post.text.includes(this.state.userinput))
+  console.log(newArr)  
+  return this.props.searchPosts(newArr)
+  }
+  
+
   render() {
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input placeholder="Search Your Feed" onChange={(e)=>this.handleChange(e.target.value)}/>
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon id="Search__icon" onClick={this.filtersPosts} />
         </div>
         
       </section>
